b8153a8060dad8b4b61f84ebee71a9673d4107c9
devarsh0804 <149481882+devarsh0804@users.noreply.github.com>
2026-07-09 17:46:31 +0530

    Add solution for Maximum Subarray

---
+++ solutions/medium/Maximum Subarray.java
@@ -0,0 +1,19 @@
+// Title: Maximum Subarray
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/maximum-subarray/
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int max=Integer.MIN_VALUE;
+        int sum=0;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            max=Math.max(max,sum);
+        }
+            if(sum<0){
+                sum=0;
+            }
+    }
+        return max;
+}
