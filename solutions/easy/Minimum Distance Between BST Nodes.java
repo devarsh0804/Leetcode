f11bca405a92e61d25eccdb2bd35c6edbaeb9fd0
devarsh0804 <149481882+devarsh0804@users.noreply.github.com>
2026-07-09 11:53:41 +0530

    Add solution for Minimum Distance Between BST Nodes

---
+++ solutions/easy/Minimum Distance Between BST Nodes.java
@@ -0,0 +1,22 @@
+// Title: Minimum Distance Between BST Nodes
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/minimum-distance-between-bst-nodes/
+
+class Solution {
+    int min = Integer.MAX_VALUE;
+ * }
+ */
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
