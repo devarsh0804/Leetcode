1378ce86beb0546fed32692dedfa487fa559c761
devarsh0804 <149481882+devarsh0804@users.noreply.github.com>
2026-07-09 17:52:39 +0530

    Add solution for Container With Most Water

---
+++ solutions/medium/Container With Most Water.java
@@ -0,0 +1,6 @@
+// Title: Container With Most Water
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/container-with-most-water/
+
+c
