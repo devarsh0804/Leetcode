8cd7a13c8fe169d1c2aed7059e996b21b9a40625
devarsh0804 <149481882+devarsh0804@users.noreply.github.com>
2026-07-10 23:15:28 +0530

    Add solution for Maximum Subarray

--- solutions/medium/Maximum Subarray.java
+++ solutions/medium/Maximum Subarray.java
@@ -10,10 +10,10 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             sum+=nums[i];
             max=Math.max(max,sum);
-        }
             if(sum<0){
                 sum=0;
             }
-    }
+        }
         return max;
+    }
 }
