9fd5ed35230ccd20be17e1228763ad9d0d812b14
devarsh0804 <149481882+devarsh0804@users.noreply.github.com>
2026-08-01 15:14:05 +0530

    Add solution for Next Permutation

---
+++ solutions/medium/Next Permutation.java
@@ -0,0 +1,22 @@
+// Title: Next Permutation
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/next-permutation/
+
+      }
+  }
+    public void nextPermutation(int[] nums) {
+        int ind=-1;
+        int n=nums.length;
+        for(int i=n-2;i>=0;i--){
+      while (start < end) {
+          swap(nums, start, end);
+          start++;
+          end--;
+      nums[j] = temp;
+  }
+  private void reverse(int[] nums, int start, int end) {
+class Solution {
+  private void swap(int[] nums, int i, int j) {
+      int temp = nums[i];
+      nums[i] = nums[j];
