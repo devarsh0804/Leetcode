0039b23b73ed6a23376703d256385ed044d1391a
devarsh0804 <149481882+devarsh0804@users.noreply.github.com>
2026-08-24 23:50:43 +0530

    Add solution for Maximum Product Subarray

---
+++ solutions/medium/Maximum Product Subarray.java
@@ -0,0 +1,22 @@
+// Title: Maximum Product Subarray
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/maximum-product-subarray/
+
+                prefix = 1;
+            }
+
+            if (suffix == 0) {
+                suffix = 1;
+            }
+
+            if (prefix == 0) {
+        for (int i = 0; i < nums.length; i++) {
+
+        int suffix = 1;
+        int max = Integer.MIN_VALUE;
+
+    public int maxProduct(int[] nums) {
+
+        int prefix = 1;
+class Solution {
