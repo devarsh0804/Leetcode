fbc8739d52c39f181777dfbf3566d38098a30a91
devarsh0804 <149481882+devarsh0804@users.noreply.github.com>
2026-09-02 21:58:06 +0530

    Add solution for Longest Common Prefix

---
+++ solutions/easy/Longest Common Prefix.java
@@ -0,0 +1,21 @@
+// Title: Longest Common Prefix
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/longest-common-prefix/
+
+
+        String prefix = strs[0];
+
+        for (int i = 1; i < strs.length; i++) {
+            while (!strs[i].startsWith(prefix)) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+
+                if (prefix.length() == 0) {
+                    return "";
+                }
+            }
+        }
+
+        return prefix;
+    }
+}
